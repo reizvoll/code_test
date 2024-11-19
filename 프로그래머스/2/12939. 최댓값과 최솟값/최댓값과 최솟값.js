80384fb955986d53977eed9222d43a7ff8d3f728
림졍 <mara2277@naver.com>
2024-11-20 02:58:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    const numbers = s.split(' ').map(Number);
+
+    let min = numbers[0];
+    let max = numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] < min) {
+            min = numbers[i];
+        }
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+
+    return `${min} ${max}`;
+}
